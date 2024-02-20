adefdab6ef989f3137961ca9b83b0e49bc72117c
Marzbani <hamied.reza.m@gmail.com>
2024-02-20 22:39:24 +0330

    [init] the project initial commit, all required libraries have been added

---
+++ android/app/src/main/kotlin/com/hamlet/shopping_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hamlet.shopping_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+
+// Main function to run the Flutter application.
+void main() async {
+  // Ensure that Flutter bindings are initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables from the .env file.
+  await dotenv.load(fileName: ".env");
+
+  // Run the application with the MyApp widget.
+  runApp(const MyApp());
+}
+
+// MyApp widget, representing the root of the application.
+class MyApp extends StatelessWidget {
+  // Constructor for the MyApp widget.
+  const MyApp({super.key});
+
+  // Build method to define the overall structure of the application.
+  @override
+  Widget build(BuildContext context) {
+    // Initialize ScreenUtil for responsive design.
+    return ScreenUtilInit(
+      designSize: const Size(360, 640),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        // Use GetMaterialApp for managing the application's state and routes.
+        return GetMaterialApp(
+          locale: Get.locale, // Set the locale for localization.
+          title: AppText.appName, // Set the application title.
+          initialBinding:
+          MainBinding(), // Use MainBinding for initial setup and dependency injection.
+          debugShowCheckedModeBanner:
+          true, // Show the debug banner in checked mode.
+          initialRoute:
+          HomeScreen.url, // Set the initial route to the HomeScreen.
+          getPages:
+          AppPages.pages, // Define the application's pages using AppPages.
+        );
+      },
+    );
+  }
+}
